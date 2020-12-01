aeebfc2ec72f5d9fb4b2a579e6492b5ba2f2f1fa
miroviazanko <miro.viazanko@gmail.com>
2020-12-01 10:37:46 +0100

    vytvoreene logo s animaciou

---
+++ src/components/logo/Logo.js
@@ -0,0 +1,15 @@
+import s from './Logo.module.scss';
+
+import logo300 from '../../assets/logo/logo300-nohand.png';
+import hand from '../../assets/logo/hand300-exp.png';
+
+export default function Logo() {
+    return(
+        <div className={s.logoContainer}>
+
+            <img src={logo300} alt="logo strojarska vyroba, konstrukcia" className={s.logoNoHand} />
+            <img src={hand} alt="logo strojarska vyroba, konstrukcia" className={s.hand} />
+
+        </div>
+    )
+}
