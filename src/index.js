2908b0bda9d4fcbcc597abaea06439cb70ae2afb
miroviazanko <miro.viazanko@gmail.com>
2020-12-23 08:51:13 +0100

    zmiznutie mobile menu po kliku na polozku

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
@@ -10,11 +10,11 @@ import reportWebVitals from './reportWebVitals';
 
 
 ReactDOM.render(
-  <HashRouter>
+  <BrowserRouter>
 
       <App />
 
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
